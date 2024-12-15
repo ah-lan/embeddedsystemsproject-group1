7b17183d7aa457752eaeebfdf9798d66fa1ed382
allan mugenyi <157706090+ah-lan@users.noreply.github.com>
2024-12-15 21:57:40 +0300

    Update masterboard.ino

--- masterboard/masterboard.ino
+++ masterboard/masterboard.ino
@@ -8,4 +8,22 @@
 #define TURBIDITY_THRESHOLD 500
 #define EEPROM_TEMPERATURE_ADDR 0 // EEPROM address for storing temperature
 #define EEPROM_TURBIDITY_ADDR 2   // EEPROM address for storing turbidity
+// LCD Pins
+#define RS 12
+#define EN 11
+#define D4 4
+#define D5 5
+#define D6 3
+#define D7 2
 
+volatile uint16_t temperatureC = 0;
+volatile uint16_t turbidity = 0;
+unsigned long lastDisplayUpdate = 0;
+const unsigned long displayInterval = 1000; // 1-second interval for LCD update
+
+#define REG(addr) (*((volatile uint8_t *)(addr)))
+#define SFR_BIT_SET(sfr, bit) (REG(sfr) |= (1 << (bit)))
+#define SFR_BIT_CLEAR(sfr, bit) (REG(sfr) &= ~(1 << (bit)))
+#define SFR_BIT_READ(sfr, bit) (REG(sfr) & (1 << (bit)))
+#define SFR_WRITE(sfr, value) (REG(sfr) = (value))
+#define SFR_READ(sfr) (REG(sfr))
